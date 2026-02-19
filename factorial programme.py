64ebf0791cba9090dc093c1de011f0d931c8e423
Omkar kumar <omkarkumar@Omkars-MacBook-Air.local>
2026-02-19 12:52:56 +0530

    Code Uploaded

---
+++ factorial programme.py
@@ -0,0 +1,7 @@
+#factorial number
+a = input("enter the value")
+f = 1
+for i in range (1,f+1):
+    f = f*i
+print(f)
+6
---
+++ omkar.py
@@ -0,0 +1,49 @@
+name = "omkar"
+age = 56
+price = 675.67
+a = None 
+old = True 
+
+print(type(name))
+print(type(age))
+print(type(price))   
+
+
+name1 = "ok"
+name2 = 'ok'
+name3 = '''ok'''
+
+print(name1)
+print(name2)
+print(name3) 
+
+
+print(type(age))
+print((type(a)))
+print(type(old))
+
+
+
+
+a = 3
+b = 3
+sum = a+b
+print(sum)
+
+
+#Taking inputs from & printing it
+name = input("name :")
+
+
+#int valus inputs
+age = int(input("age :"))
+
+name = input("name :")
+age = int(input("age :"))
+price = float(input("price : "))
+
+print("My name is", name, "and i am",age, "years old")
+
+
+
+
